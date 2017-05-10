6e0b26f8ec18b83dc2761eb1d316b83d59985e67
blackDoe <blackdoe@mailoo.org>
2017-05-10 17:11:18 +0200

    Correction / Identation / Clarification. Voilà un code propre. s'y référer pour les convention. Regarder les commentaires

--- CF/main.cpp
+++ CF/main.cpp
@@ -8,70 +8,59 @@
 #include "dct.h"
 
 
-int SaveIntImage_pgm(char *nom, int **im, int Height, int Width)
-{
+int SaveIntImage_pgm(char *nom, int **im, int Height, int Width) {
     int i, j;
 
-    unsigned char ** ima_uc = alocamuc(Height, Width);
+    unsigned char **ima_uc = alocamuc(Height, Width);
 
     int maxim = 0;
 
-    for(i = 0; i<Height; i++)
-        for(j = 0; j<Width; j++)
-        {
-            if(abs(im[i][j]) > maxim)
+    for (i = 0; i < Height; i++)
+        for (j = 0; j < Width; j++)
+            if (abs(im[i][j]) > maxim)
                 maxim = abs(im[i][j]);
-        }
+
     //  fprintf(stderr, "\nnom = %s  maxim = %d\n", nom, maxim);
 
-    for(i = 0; i<Height; i++)
-        for(j = 0; j<Width; j++)
-        {
-            ima_uc[i][j] = (unsigned char) ( (abs(im[i][j]) *255)/ maxim);
-        }
+    for (i = 0; i < Height; i++)
+        for (j = 0; j < Width; j++)
+            ima_uc[i][j] = (unsigned char)(abs(im[i][j]) * 255 / maxim);
 
     ecriture_pgm(nom, ima_uc, Width, Height);
 
     dalocuc(ima_uc, Height);
 
-    return 1;
+    return 1;   //EXIT_SUCCESS or EXIT_FAILURE is better
 }
 
-
-int SaveIntImage_pgm_tronc(char *nom, int **im, int Height, int Width)
-{
+int SaveIntImage_pgm_tronc(char *nom, int **im, int Height, int Width) {
     int i, j;
 
-    unsigned char ** ima_uc = alocamuc(Height, Width);
+    unsigned char **ima_uc = alocamuc(Height, Width);
 
-
-    for(i = 0; i<Height; i++)
-        for(j = 0; j<Width; j++)
+    for (i = 0; i < Height; i++)
+        for (j = 0; j < Width; j++)
         {
-            if( abs(im[i][j]) >= 255.0 )
+            if (abs(im[i][j]) >= 255.0)
                 ima_uc[i][j] = 255;
             else
-                ima_uc[i][j] = (unsigned char) ( (abs(im[i][j])) );
+                ima_uc[i][j] = (unsigned char)(abs(im[i][j]));
         }
 
     ecriture_pgm(nom, ima_uc, Width, Height);
 
     dalocuc(ima_uc, Height);
 
-    return 1;
+    return 1;   //EXIT_SUCCESS or EXIT_FAILURE is better
 }
 
-
-int main (int argc, char *argv[])
-{
+int main (int argc, char *argv[]) {
     char nom[200], nom_out[200], nom_err[300], nom_out1bis[200], nom_err1bis[300];
-    int W, H; // les dimensions de li'image: H = Height, W = Width
+    int W, H; // les dimensions de l'image: H = Height, W = Width
     int i, j;
 
     if (argc != 3)
-    {
         fprintf(stderr, "Utilisation: %s <nom_image_pgm> <pas_quantification> \n", argv[0]); exit(0);
-    }
 
     strcpy(nom, argv[1]);
     strcpy(nom_out, argv[1]); strcat(nom_out, ".out");
@@ -96,363 +85,312 @@ int main (int argc, char *argv[])
     int **err = alocami(H, W); // allocation de la matric des erreurs de prediction
     int **err1bis = alocami(H, W);
 
-    for(i=0;i<H;i++)
-        for(j=0;j<W;j++)
-            err[i][j] = (int)x[i][j];
+    for (i = 0; i < H; i++)
+        for (j = 0; j < W; j++)
+            err[i][j] = (int)(x[i][j]);
 
-    for(i=0;i<H;i++)
-        for(j=0;j<W;j++)
-            err1bis[i][j] = (int)x[i][j];
+    for (i = 0; i < H; i++)
+        for (j = 0; j < W; j++)
+            err1bis[i][j] = (int)(x[i][j]);
 
     fprintf(stderr, "\nentropie initiale = %g [bits/pixel]\n", calc_entropie(err, H, W));
 
-
-
     // CODE DCT
-    double **tdct = alocamd(H, W); // image transformee
-    double **xd = alocamd(H, W);   // image initiale en double
 
+    double **tdct = alocamd(H, W);  // image transformee
+    double **xd = alocamd(H, W);    // image initiale en double
 
-    for(i=0; i<H; i++)
-        for(j=0; j<W; j++)
-            xd[i][j] = (double)x[i][j];
-
+    for (i = 0; i < H; i++)
+        for (j = 0; j < W; j++)
+            xd[i][j] = (double)(x[i][j]);
 
     dct2dim(xd, tdct, H, W);
     fprintf(stderr, "OK DCT directe\n");
 
-
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     double **matDct1= alocamd(H, W);
 
-    for(j=0; j<W; j++)
-        matDct1[0][j] = 1/(sqrt(H));
-    for(i=1; i<H; i++){
-        for(j=0; j<W; j++){
-            matDct1[i][j]= (sqrt(2.)/sqrt((double) H)) * cos(M_PI * (2 * j + 1) * i / (2 * H));
-        }
-    }
+    for (j = 0; j < W; j++)
+        matDct1[0][j] = 1/sqrt(H);
 
+    for (i = 1; i < H; i++)
+        for(j = 0; j < W; j++)
+            matDct1[i][j] = sqrt(2./(double)(H)) * cos(M_PI * (2*j + 1) * i / (2 * H)); // Convention à définir
 
     double **matDct2 = alocamd(W, H);
-    for(i=0;i<H;i++){
-        matDct2[i][0] = 1/(sqrt(W));
-        for(j=1;j<W;j++){
-            matDct2[i][j]= (sqrt(2.)/sqrt((double) W))*cos(M_PI*(2*i+1)*j/(2*W));
-        }
+    for (i = 0; i < H; i++) {
+        matDct2[i][0] = 1/sqrt(W);
+        for (j = 1; j < W; j++)
+            matDct2[i][j] = sqrt(2./(double)(W))*cos(M_PI*(2*i+1)*j/(2*W)); // Convention à définir (voir 132)
     }
 
+    // À définir: c'est quoi?
     /*
-    for(i=0;i<H;i++)
-        for(j=0;j<W;j++)
+    for (i = 0; i < H; i++)
+        for (j = 0; j < W; j++)
             matDct2[i][j] = matDct1[j][i];
-   */
+    */
 
-
-    ////////////////////////////////////////      TEST 1      ///////////////////////////////////////////////////////
-
-/*
+    ////////////////////////////////////////    TEST 1  ////////////////////////////////////////////////////////////////
+    /*
     double **tdct1 = alocamd(H, W); // image transformee test 1
 
-
-
-    int a;
-    int b;
-    for (a=0;a<W;a++) {
-        for (b = 0; b < H; b++) {
-            for (i = 0; i < H; i++) {
-                for (j = 0; j < W; j++) {
+    int a, b;
+    for (a = 0; a < W; a++) {
+        for (b = 0; b < H; b++)
+            for (i = 0; i < H; i++)
+                for (j = 0; j < W; j++)
                     tdct1[a][b] += matDct1[i][a] * matDct2[b][j] * xd[i][j];
-                }
-            }
-        }
         fprintf(stderr, "a = %d / %d \n",a,H-1);
     }
+    */
 
-*/
+    ////////////////////////////////////////    TEST 1 bis  ////////////////////////////////////////////////////////////
 
+    double **tdct1b = alocamd(H, W); // image transition test 1bis
+    double **tdct1bis = alocamd(H, W);   // image transformee test 1bis
 
-    ////////////////////////////////////    TEST 1 bis     ///////////////////////////////////////////////////
-
-
-
-    double **tdct1b = alocamd(H,W); // image transition test 1bis
-    double **tdct1bis = alocamd(H,W); // image transformee test 1bis
-
-    for(i=0;i<H;i++)
-        for(j=0;j<W;j++){
+    for (i = 0; i < H; i++)
+        for(j = 0; j < W; j++) {
             tdct1b[i][j] = 0.0;
             tdct1bis[i][j] = 0.0;
         }
 
     int aa, bb, ii;
 
-    for (aa=0; aa<H; aa++)
+    for (aa = 0; aa < H; aa++)
         for (bb = 0; bb <W; bb++)
             for (ii = 0; ii < H; ii++)
                 tdct1b[aa][bb] += matDct1[aa][ii] * xd[ii][bb];
 
-
-
     int jj;
-    for (aa=0; aa<H; aa++) {
-        for (bb = 0; bb <W; bb++) {
-            for (jj = 0; jj < W; jj++) {
-                tdct1bis[aa][bb] += tdct1b[aa][jj] * matDct2[jj][bb] ;
-            }
-        }
-    }
+
+    for (aa = 0; aa < H; aa++)
+        for (bb = 0; bb <W; bb++)
+            for (jj = 0; jj < W; jj++)
+                tdct1bis[aa][bb] += tdct1b[aa][jj] * matDct2[jj][bb];
 
     fprintf(stderr, "DCT1bis directe effectuée\n");
 
 
+    //  Exemple de produit matriciel classique C = A*B
+    /*  avec C de taille (H, W) , A de taille (H, K) et B de taille (K, W)
 
-
-
-    // Exemple de produit matriciel classique C = A*B
-    // avec C de taille (H,W) , A de taille (H,K) et B de taille (K,W)
-
-    /*
-
-    for (i=0;i<H;i++)
-        for (j=0;j<W;j++)
-        {
+    for (i = 0; i < H; i++)
+        for (j = 0; j < W; j++) {
             int z;
-            for (z=0;z<K;z++)
+            for (z = 0; z < K; z++)
                 C[i][j] += A[i][z] * B[z][j];
         }
 
     */
 
-    /////////////////////////////////////    TEST 2     /////////////////////////////////////////////////////
-
-/*
+    ////////////////////////////////////////////////////   TEST 2  /////////////////////////////////////////////////////
+    // Il faut clairement le refaire. Ou le laisser tomber
+    /*
     double **tdct2 = alocamd(H, W); // image transformee test 2
 
+    int u, v;
+    int x2, y2;
 
-    int u;
-    int v;
-    int y2;
-    int x2;
-    for(u=0;u<H;u++){
-        for(v=0;v<W;v++){
-            for(y2=0;y2<W;y2++){
-                for(x2=0;x2<H;x2++){
-                 //   if(u=0){
+    for ( u = 0; u < H; u++) {
+        for(v = 0; v < W; v++)
+            for(y2 = 0; y2 < W; y2++)
+                for(x2 = 0; x2 < H; x2++) {
+                 //   if (u = 0)
                  //      tdct2[u][v] += (sqrt(2/(H*W)))*xd[x2][y2]*cos((2*x2+1)*u*M_PI/(2*H))*cos((2*y2+1)*v*M_PI/(2*W));
-                 //   }
-                 //   if(v=0){
+                 //   if (v = 0)
                  //       tdct2[u][v] += (sqrt(2/(H*W)))*xd[x2][y2]*cos((2*x2+1)*u*M_PI/(2*H))*cos((2*y2+1)*v*M_PI/(2*W));
-                 //   }
                     tdct2[u][v] += (2/(sqrt(H*W)))*xd[x2][y2]*cos((2*x2+1)*u*M_PI/(2*H))*cos((2*y2+1)*v*M_PI/(2*W));
                 }
-            }
-        }
         fprintf(stderr, "u = %d / %d \n",u,H-1);
     }
 
 */
 
-    ////////////////////////////     Comparaison avec la "vraie" dct      ////////////////////////////////////
-
-/*
+    ///////////////////////////////////////    Comparaison avec la "vraie" dct  ////////////////////////////////////////
+    /*
     fprintf(stderr, "\ntdct[3] = \n[");
-    for(j = 0; j < W; j++) {
+    for(j = 0; j < W; j++)
         fprintf(stderr, " %g ", tdct[3][j]);
-    }
-    fprintf(stderr, "]\n");
-*/
 
-/*
+    fprintf(stderr, "]\n");
+    */
+    /*
     fprintf(stderr, "\ntdct1[3] = \n[");
-    for(j = 0; j < W; j++) {
+    for(j = 0; j < W; j++)
         fprintf(stderr, " %g ", tdct1[3][j]);
-    }
-    fprintf(stderr, "]\n");
-*/
 
-/*
+    fprintf(stderr, "]\n");
+    */
+    /*
     fprintf(stderr, "\ntdct1bis[3] = \n[");
-    for(j = 0; j < W; j++) {
+    for(j = 0; j < W; j++)
         fprintf(stderr, " %f ", tdct1bis[3][j]);
-    }
+
     fprintf(stderr, "]\n");
-*/
-
-/*
-
+    */
+    /*
     fprintf(stderr, "\ntdct2[3] = \n[");
-    for(j = 0; j < W; j++) {
+    for(j = 0; j < W; j++)
         fprintf(stderr, " %g ", tdct2[3][j]);
-    }
+
     fprintf(stderr, "]\n");
+    */
 
-*/
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    //////////////////////////////////////////////////////////////////////////////////////////////////////
+    //  Quantification des coefficients de la DCT ... avec tdct
 
-
-//Quantification des coefficients de la DCT ... avec tdct
-    for(i=1;i<H;i++)
-        for(j=1;j<W;j++)
-        {
+    for (i = 1; i < H; i++)
+        for (j = 1; j < W; j++) {
             err[i][j] = quantiz(tdct[i][j], step);
-            tdct[i][j] = (double)err[i][j];
+            tdct[i][j] = (double)(err[i][j]);
         }
-    for(j=0;j<W;j++)
-    {err[0][j] =  quantiz(tdct[0][j], 1); tdct[0][j] = (double)err[0][j];}
-    for(i=0;i<H;i++)
-    {err[i][0] =  quantiz(tdct[i][0], 1); tdct[i][0] = (double)err[i][0];}
+    for (j = 0; j < W; j++)
+        err[0][j] = quantiz(tdct[0][j], 1); tdct[0][j] = (double)(err[0][j]);
+    for (i = 0; i < H; i++)
+        err[i][0] = quantiz(tdct[i][0], 1); tdct[i][0] = (double)(err[i][0]);
 
+    //  Quantification des coefficients de la DCT ... avec tdct1bis
 
-//Quantification des coefficients de la DCT ... avec tdct1bis
-    for(i=1;i<H;i++)
-        for(j=1;j<W;j++)
-        {
+    for (i = 1; i < H; i++)
+        for(j = 1; j < W; j++) {
             err1bis[i][j] = quantiz(tdct1bis[i][j], step);
-            tdct1bis[i][j] = (double)err1bis[i][j];
+            tdct1bis[i][j] = (double)(err1bis[i][j]);
         }
-    for(j=0;j<W;j++)
-    {err1bis[0][j] =  quantiz(tdct1bis[0][j], 1); tdct1bis[0][j] = (double)err1bis[0][j];}
-    for(i=0;i<H;i++)
-    {err1bis[i][0] =  quantiz(tdct1bis[i][0], 1); tdct1bis[i][0] = (double)err1bis[i][0];}
+    for (j = 0; j < W; j++)
+        err1bis[0][j] =  quantiz(tdct1bis[0][j], 1); tdct1bis[0][j] = (double)(err1bis[0][j]);
+    for (i = 0; i < H; i++)
+        err1bis[i][0] =  quantiz(tdct1bis[i][0], 1); tdct1bis[i][0] = (double)(err1bis[i][0]);
 
-    //FIN Quantification des coefficients de la DCT
-
+    //  FIN Quantification des coefficients de la DCT
 
     double entro = calc_entropie(err, H, W);
+
     fprintf(stderr, "\nentropie = %g [bits/pixel]\n", entro);
 
-
     dct2dim_inv(tdct, xrecd, H, W);
-    for(i=0;i<H;i++)
-        for(j=0;j<W;j++)
-            if(xrecd[i][j] < 0.0)  xrec[i][j] = 0;
-            else if(xrecd[i][j] > 255.0)  xrec[i][j] = 255;
-            else  xrec[i][j] = (unsigned char)xrecd[i][j];
+
+    for (i = 0; i < H; i++)
+        for (j = 0; j < W; j++)
+            if (xrecd[i][j] < 0.0)
+                xrec[i][j] = 0;
+            else if (xrecd[i][j] > 255.0)
+                xrec[i][j] = 255;
+            else
+                xrec[i][j] = (unsigned char)(xrecd[i][j]);
+
     SaveIntImage_pgm_tronc(nom_err, err, H, W);
+
     ecriture_pgm(nom_out, xrec, W, H);
 
-
-    /////////////////////////////// dct2dim_inv  TEST1bis   /////////////////////////////////////////////////
+    ////////////////////////////////////////// dct2dim_inv  TEST1bis    ////////////////////////////////////////////////
 
     double **xrecd1b = alocamd(H, W);
     double **xrecd1bis = alocamd(H, W);
-    for(i=0;i<H;i++)
-        for(j=0;j<W;j++)
-        {
+
+    for(i = 0; i < H; i++)
+        for(j = 0; j < W; j++) {
             xrecd1b[i][j] = 0.0;
             xrecd1bis[i][j] = 0.0;
         }
 
+    double **matDct1Inv = alocamd(W, H);
 
-    double **matDct1Inv = alocamd(W,H);
-
-    for(i=0;i<H;i++)
-        for(j=0;j<W;j++)
+    for (i = 0; i < H; i++)
+        for (j = 0; j < W; j++)
             matDct1Inv[i][j] = matDct1[j][i];
 
-    double **matDct2Inv = alocamd(H,W);
+    double **matDct2Inv = alocamd(H, W);
 
-    for(i=0;i<H;i++)
-        for(j=0;j<W;j++)
+    for(i = 0; i < H; i++)
+        for(j = 0; j < W; j++)
             matDct2Inv[i][j] = matDct2[j][i];
 
-
-    for (aa=0; aa<H; aa++)
-        for (bb = 0; bb <W; bb++)
+    for (aa = 0; aa < H; aa++)
+        for (bb = 0; bb < W; bb++)
             for (ii = 0; ii < H; ii++)
                 xrecd1b[aa][bb] += matDct1Inv[aa][ii] * tdct1bis[ii][bb];
 
-
-    for (aa=0; aa<H; aa++) {
-        for (bb = 0; bb <W; bb++) {
-            for (jj = 0; jj < W; jj++) {
-                xrecd1bis[aa][bb] += xrecd1b[aa][jj] * matDct2Inv[jj][bb] ;
-            }
-        }
-    }
+    for (aa = 0; aa < H; aa++)
+        for (bb = 0; bb <W; bb++)
+            for (jj = 0; jj < W; jj++)
+                xrecd1bis[aa][bb] += xrecd1b[aa][jj] * matDct2Inv[jj][bb];
 
     fprintf(stderr, "DCT1bis inverse effectué\n");
 
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    for (i = 0; i < H; i++)
+        for (j = 0; j < W; j++)
+            if (xrecd1bis[i][j] < 0.0)
+                xrec1bis[i][j] = 0;
+            else if (xrecd1bis[i][j] > 255.0)
+                xrec1bis[i][j] = 255;
+            else
+                xrec1bis[i][j] = (unsigned char)(xrecd1bis[i][j]);
 
-
-    for(i=0;i<H;i++)
-        for(j=0;j<W;j++)
-            if(xrecd1bis[i][j] < 0.0)  xrec1bis[i][j] = 0;
-            else if(xrecd1bis[i][j] > 255.0)  xrec1bis[i][j] = 255;
-            else  xrec1bis[i][j] = (unsigned char)xrecd1bis[i][j];
     SaveIntImage_pgm_tronc(nom_err1bis, err1bis, H, W);
     ecriture_pgm(nom_out1bis, xrec1bis, W, H);
 
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    //////////////////////////////////////////////////////////////////////////////////////////////
+    dalocd(xrecd, H);
+    dalocd(xd, H);
+    dalocd(tdct, H);
+    //
+    dalocd(xrecd1bis, H);
+    dalocd(tdct1bis, H);
 
-    dalocd(xrecd,H);
-    dalocd(xd,H);
-    dalocd(tdct,H);
+    //  FIN CODE DCT
 
-    dalocd(xrecd1bis,H);
-    dalocd(tdct1bis,H);
+    //  DCT PAR BLOCS
+    /*
+    double **tdct = alocamd(H, W);    // image transformee
+    double **xd = alocamd(H, W);      // image initiale en double
+    double **xrecd = alocamd(H, W);   // image reconstruite
 
-    //FIN CODE DCT
+    for (i = 0; i < H; i++)
+        for (j = 0; j < W; j++)
+            xd[i][j] = (double)(x[i][j]);
 
+    int Bx=4, By=4;
 
-/*
-  // DCT PAR BLOCS
+    dct2dim_bloc(xd, tdct, H, W, Bx, By, step);
 
-  double **tdct = alocamd(H, W); // image transformee
-  double **xd = alocamd(H, W);   // image initiale en double
-  double **xrecd = alocamd(H, W); // image reconstruite
+    for (i = 0; i < H; i++)
+        for(j = 0; j < W; j++)
+            err[i][j] = (int)(tdct[i][j]);
 
-    for(i=0;i<H;i++)
-      for(j=0;j<W;j++)
-        xd[i][j] = (double)x[i][j];
+    double entro = calc_entropie(err, H, W);
 
+    fprintf(stderr, "\nentropie = %g [bits/pixel]\n", entro);
 
+    dct2dim_bloc_inv(tdct, xrecd, H, W, Bx, By);
 
-  int Bx=4, By=4;
-
-  dct2dim_bloc(xd, tdct, H, W, Bx, By, step);
-
-  for(i=0;i<H;i++)
-    for(j=0;j<W;j++)
-      err[i][j] = (int)tdct[i][j];
-
-  double entro = calc_entropie(err, H, W);
-  fprintf(stderr, "\nentropie = %g [bits/pixel]\n", entro);
-
-  dct2dim_bloc_inv(tdct, xrecd, H, W, Bx, By);
-
-  for(i=0;i<H;i++)
-      for(j=0;j<W;j++)
-        if(xrecd[i][j] < 0.0)  xrec[i][j] = 0;
-        else if(xrecd[i][j] > 255.0)  xrec[i][j] = 255;
-        else  xrec[i][j] = (unsigned char)xrecd[i][j];
+    for(i = 0; i < H; i++)
+        for(j = 0; j < W; j++)
+            if (xrecd[i][j] < 0.0)
+                xrec[i][j] = 0;
+            else if (xrecd[i][j] > 255.0)
+                xrec[i][j] = 255;
+            else
+                xrec[i][j] = (unsigned char)(xrecd[i][j]);
 
     SaveIntImage_pgm_tronc(nom_err, err, H, W);
     ecriture_pgm(nom_out, xrec, W, H);
-     dalocd(xrecd,H);
-    dalocd(xd,H);
-    dalocd(tdct,H);
+    dalocd(xrecd, H);
+    dalocd(xd, H);
+    dalocd(tdct, H);
+    */
+    //  FIN DCT PAR BLOCS
 
+    //  PREDICTION
 
-  // FIN DCT PAR BLOCS
-*/
-//
-
-
-//PREDICTION
-
-
-
-    // MyCodeur(x, H, W, step, err);
-    // MyDecodeur(xrec, H, W, err);
-
+    //MyCodeur(x, H, W, step, err);
+    //MyDecodeur(xrec, H, W, err);
 
     //codeur_adapt(x, err, H, W, step);
     //decodeur_adapt(err, xrec, H, W);
@@ -462,14 +400,13 @@ int main (int argc, char *argv[])
     fprintf(stderr, "\nentropie = %g [bits/pixel]\n", entro);
     SaveIntImage_pgm(nom_err, err, H, W);
 
-
     ecriture_pgm(nom_out, xrec, W, H);
 
-//FIN PREDICTION
+    //FIN PREDICTION
 
-    dalocuc(x,H);
-    dalocuc(xrec,H);
-    daloci(err,H);
+    dalocuc(x, H);
+    dalocuc(xrec, H);
+    daloci(err, H);
 
     return 1;
 }
