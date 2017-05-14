b3c585d35c5859e69a2cd52ba9ca045fb7872eea
blackDoe <blackdoe@mailoo.org>
2017-05-14 17:20:02 +0200

    Fonction relues, tdst implémentée non testée. On s'attaque au main dans le prochain commit

--- CF/main.cpp
+++ CF/main.cpp
@@ -9,10 +9,9 @@
 
 
 int SaveIntImage_pgm(char *nom, int **im, int Height, int Width) {
+
     int i, j;
-
     unsigned char **ima_uc = alocamuc(Height, Width);
-
     int maxim = 0;
 
     for (i = 0; i < Height; i++)
@@ -29,15 +28,14 @@ int SaveIntImage_pgm(char *nom, int **im, int Height, int Width) {
     ecriture_pgm(nom, ima_uc, Width, Height);
 
     dalocuc(ima_uc, Height);
-
-    return 1;   //EXIT_SUCCESS or EXIT_FAILURE is better
+    return EXIT_FAILURE;
 }
 
 int SaveIntImage_pgm_tronc(char *nom, int **im, int Height, int Width) {
+
     int i, j;
 
     unsigned char **ima_uc = alocamuc(Height, Width);
-
     for (i = 0; i < Height; i++)
         for (j = 0; j < Width; j++)
         {
@@ -50,55 +48,43 @@ int SaveIntImage_pgm_tronc(char *nom, int **im, int Height, int Width) {
     ecriture_pgm(nom, ima_uc, Width, Height);
 
     dalocuc(ima_uc, Height);
-
-    return 1;   //EXIT_SUCCESS or EXIT_FAILURE is better
+    return EXIT_FAILURE;
 }
 
+void My_dct2dim(double **xd, double **tdct1, int H, int W) {
 
-void My_dct2dim(double **xd, double **tdct1, int H, int W){
-
-    // Valeurs utiles : on ne les calcule qu'une fois car long
-    double sqrth = sqrt((double)H);
-    double sqrtw = sqrt((double)W);
+    // Valeurs utiles : on ne les calcule qu'une fois car long à écrire
+    double sqrth = sqrt((double)(H));
+    double sqrtw = sqrt((double)(W));
     double sqrt2 = sqrt(2.);
 
-    // Matrices utiles
-    int i, j;
-    double **matDct1= alocamd(H, W);
+    int i, j;   // Pour la définition des matrices utiles
 
-    for ( j = 0; j < W; j++)
+    double **matDct1 = alocamd(H, W);
+    for (j = 0; j < W; j++)
         matDct1[0][j] = 1 / sqrth;
-    for( i = 1 ; i < H; i++){
-        for(j = 0; j < W; j++){
-            matDct1[i][j]= (sqrt2 / sqrth) * cos(M_PI * (2 * j + 1) * i / (2*H));
-        }
-    }
-
+    for (i = 1 ; i < H; i++)
+        for (j = 0; j < W; j++)
+            matDct1[i][j] = (sqrt2/sqrth) * cos(M_PI * (2*j + 1) * i / (2*H));
 
     double **matDct2 = alocamd(W, H);
-
-    for ( i = 0; i < H; i++){
-        matDct2[i][0] = 1 / sqrtw;
-        for ( j = 1; j < W; j++){
-            matDct2[i][j]= (sqrt2 / sqrtw) * cos(M_PI * (2 * i + 1) * j / (2*W));
-        }
-    }
-
-    double **tdct0 = alocamd(H, W); // image transition
-
     for (i = 0; i < H; i++)
-        for(j = 0; j < W; j++) {
-            tdct0[i][j] = 0.0;
-            tdct1[i][j] = 0.0;
-        }
+        matDct2[i][0] = 1 / sqrtw;
+        for (j = 1; j < W; j++)
+            matDct2[i][j] = (sqrt2/sqrtw) * cos(M_PI * (2*i + 1) * j / (2*W));
+
+    // Image transition
+    double **tdct0 = alocamd(H, W);
+    for (i = 0; i < H; i++)
+        for (j = 0; j < W; j++)
+            tdct0[i][j] = tdct1[i][j] = 0.0;
 
     int a, b;
 
     for (a = 0; a < H; a++)
-        for (b = 0; b <W; b++)
+        for (b = 0; b < W; b++)
             for (i = 0; i < H; i++)
                 tdct0[a][b] += matDct1[a][i] * xd[i][b];
-
 
     for (a = 0; a < H; a++)
         for (b = 0; b < W; b++)
@@ -107,45 +93,35 @@ void My_dct2dim(double **xd, double **tdct1, int H, int W){
 
 }
 
+void My_dct2dim_inv(double **tdct1, double **xrecd1, int H, int W) {
 
-void My_dct2dim_inv(double **tdct1, double **xrecd1, int H, int W){
-
-    // Valeurs utiles : on ne les calcule qu'une fois car long
-    double sqrth = sqrt((double)H);
-    double sqrtw = sqrt((double)W);
+    // Valeurs utiles : on ne les calcule qu'une fois car long à écrire
+    double sqrth = sqrt((double)(H));
+    double sqrtw = sqrt((double)(W));
     double sqrt2 = sqrt(2.);
 
-    // Matrices utiles
-    int i, j;
-    double **matDct1Inv = alocamd(W, H);        // transposée de matDct1
+    int i, j;   // Pour la définition des matrices utiles
 
-    for ( j = 0; j < W; j++)
+    // Transposée de matDct1
+    double **matDct1Inv = alocamd(W, H);
+    for (j = 0; j < W; j++)
         matDct1Inv[j][0] = 1 / sqrth;
-    for( i = 1 ; i < H; i++){
-        for(j = 0; j < W; j++){
-            matDct1Inv[j][i]= (sqrt2 / sqrth) * cos(M_PI * (2 * j + 1) * i / (2*H));
-        }
-    }
+    for (i = 1 ; i < H; i++)
+        for(j = 0; j < W; j++)
+            matDct1Inv[j][i] = (sqrt2/sqrth) * cos(M_PI * (2*j + 1) * i / (2*H));
 
-
-    double **matDct2Inv = alocamd(H, W);        // transposée de matDct2
-
-    for ( i = 0; i < H; i++){
+    // Transposée de matDct2
+    double **matDct2Inv = alocamd(H, W);
+    for (i = 0; i < H; i++) {
         matDct2Inv[0][i] = 1 / sqrtw;
-        for ( j = 1; j < W; j++){
-            matDct2Inv[j][i]= (sqrt2 / sqrtw) * cos(M_PI * (2 * i + 1) * j / (2*W));
-        }
+        for (j = 1; j < W; j++)
+            matDct2Inv[j][i] = (sqrt2/sqrtw) * cos(M_PI * (2*i + 1) * j / (2*W));
     }
-
 
     double **xrecd0 = alocamd(H, W);
-
-    for(i = 0; i < H; i++)
-        for(j = 0; j < W; j++) {
-            xrecd0[i][j] = 0.0;
-            xrecd1[i][j] = 0.0;
-        }
-
+    for (i = 0; i < H; i++)
+        for (j = 0; j < W; j++)
+            xrecd0[i][j] = xrecd1[i][j] = 0.0;
 
     int a, b;
 
@@ -154,35 +130,113 @@ void My_dct2dim_inv(double **tdct1, double **xrecd1, int H, int W){
             for (i = 0; i < H; i++)
                 xrecd0[a][b] += matDct1Inv[a][i] * tdct1[i][b];
 
-
     for (a = 0; a < H; a++)
         for (b = 0; b <W; b++)
             for (j = 0; j < W; j++)
                 xrecd1[a][b] += xrecd0[a][j] * matDct2Inv[j][b];
+
 }
 
+void My_dst2dim(double **xd, double **tdst1, int H, int W) {
 
-void quantification(double **tdct, int **err, int H, int W, int step){
+    // Valeurs utiles : on ne les calcule qu'une fois car long à écrire
+    double sqrtfh = sqrt(2./(double)(H+1));
+    double sqrtfw = sqrt(2./(double)(W+1));
+
+    int i, j;   // Pour la définition des matrices utiles
+
+    double **matDst1 = alocamd(H, W);
+    for (i = 0 ; i < H; i++)
+        for (j = 0; j < W; j++)
+            matDst1[i][j] = sqrtfh * sin(M_PI * (j+1) * (i+1) / (H+1));
+
+    double **matDst2 = alocamd(W, H);
+    for (i = 0; i < H; i++)
+        for (j = 0; j < W; j++)
+            matDst2[i][j] = sqrtfw * sin(M_PI * (i+1) * (j+1) / (W+1));
+
+    // Image transition
+    double **tdst0 = alocamd(H, W);
+    for (i = 0; i < H; i++)
+        for (j = 0; j < W; j++)
+            tdst0[i][j] = tdst1[i][j] = 0.0;
+
+    int a, b;
+
+    for (a = 0; a < H; a++)
+        for (b = 0; b < W; b++)
+            for (i = 0; i < H; i++)
+                tdst0[a][b] += matDst1[a][i] * xd[i][b];
+
+    for (a = 0; a < H; a++)
+        for (b = 0; b < W; b++)
+            for (j = 0; j < W; j++)
+                tdst1[a][b] += tdst0[a][j] * matDst2[j][b];
+
+}
+
+void My_dst2dim_inv(double **tdst1, double **xrecd1, int H, int W) {
+
+    // Valeurs utiles : on ne les calcule qu'une fois car long à écrire
+    double sqrtfh = sqrt(2./(double)(H+1));
+    double sqrtfw = sqrt(2./(double)(W+1));
+
+    int i, j;   // Pour la définition des matrices utiles
+
+    // Transposée de matDst1
+    double **matDst1Inv = alocamd(W, H);
+    for (i = 0 ; i < H; i++)
+        for(j = 0; j < W; j++)
+            matDst1Inv[j][i] = sqrtfh * sin(M_PI * (j+1) * (i+1) / (H+1));
+
+    // Transposée de matDst2
+    double **matDst2Inv = alocamd(H, W);
+    for (i = 0; i < H; i++)
+        for (j = 0; j < W; j++)
+            matDst2Inv[j][i] = sqrtfw * sin(M_PI * (i+1) * (j+1) / (W+1));
+
+    double **xrecd0 = alocamd(H, W);
+    for(i = 0; i < H; i++)
+        for(j = 0; j < W; j++)
+            xrecd0[i][j] = xrecd1[i][j] = 0.0;
+
+    int a, b;
+
+    for (a = 0; a < H; a++)
+        for (b = 0; b < W; b++)
+            for (i = 0; i < H; i++)
+                xrecd0[a][b] += matDst1Inv[a][i] * tdst1[i][b];
+
+    for (a = 0; a < H; a++)
+        for (b = 0; b <W; b++)
+            for (j = 0; j < W; j++)
+                xrecd1[a][b] += xrecd0[a][j] * matDst2Inv[j][b];
+
+}
+
+void quantification(double **tdt, int **err, int H, int W, int step) {
 
     int i, j;
+
     for (i = 1; i < H; i++)
         for (j = 1; j < W; j++) {
-            err[i][j] = quantiz(tdct[i][j], step);
-            tdct[i][j] = (double)(err[i][j]);
+            err[i][j] = quantiz(tdt[i][j], step);
+            tdt[i][j] = (double)(err[i][j]);
         }
+
     for (j = 0; j < W; j++) {
-        err[0][j] = quantiz(tdct[0][j], 1);
-        tdct[0][j] = (double)(err[0][j]);
+        err[0][j] = quantiz(tdt[0][j], 1);
+        tdt[0][j] = (double)(err[0][j]);
     }
+
     for (i = 0; i < H; i++) {
-        err[i][0] = quantiz(tdct[i][0], 1);
-        tdct[i][0] = (double)(err[i][0]);
+        err[i][0] = quantiz(tdt[i][0], 1);
+        tdt[i][0] = (double)(err[i][0]);
     }
 
 }
 
-
-void finale(double **xrecd, unsigned char **xrec, int H, int W){
+void finale(double **xrecd, unsigned char **xrec, int H, int W) {
 
     int i, j;
     for (i = 0; i < H; i++)
@@ -192,50 +246,41 @@ void finale(double **xrecd, unsigned char **xrec, int H, int W){
             else if (xrecd[i][j] > 255.0)
                 xrec[i][j] = 255;
             else
-                xrec[i][j] = (unsigned char) (xrecd[i][j]);
+                xrec[i][j] = (unsigned char)(xrecd[i][j]);
         }
 }
 
-
-void My_dct2dim_bloc(double **xd, double **tdct1, int H, int W, int Bx, int By, int step){
+void My_dct2dim_bloc(double **xd, double **tdct1, int H, int W, int Bx, int By, int step) {
 
     // Valeurs utiles : on ne les calcule qu'une fois car long
-    double sqrtBx = sqrt((double)Bx);
-    double sqrtBy = sqrt((double)By);
+    double sqrtBx = sqrt((double)(Bx));
+    double sqrtBy = sqrt((double)(By));
     double sqrt2 = sqrt(2.);
 
-    // Matrices utiles
-    int i, j;
-    double **matDct1= alocamd(Bx, By);
+    int i, j;   // Pour la définition des matrices utiles
 
-    for ( j = 0; j < By; j++)
+    double **matDct1= alocamd(Bx, By);
+    for (j = 0; j < By; j++)
         matDct1[0][j] = 1 / sqrtBx;
-    for( i = 1 ; i < Bx; i++){
-        for(j = 0; j < By; j++){
-            matDct1[i][j]= (sqrt2 / sqrtBx) * cos(M_PI * (2 * j + 1) * i / (2*Bx));
-        }
-    }
+    for (i = 1; i < Bx; i++)
+        for(j = 0; j < By; j++)
+            matDct1[i][j] = (sqrt2/sqrtBx) * cos(M_PI * (2*j + 1) * i / (2*Bx));
 
     double **matDct2 = alocamd(By, Bx);
-
-    for ( i = 0; i < Bx; i++){
+    for (i = 0; i < Bx; i++) {
         matDct2[i][0] = 1/sqrtBy;
-        for ( j = 1; j < By; j++){
-            matDct2[i][j]= (sqrt2 / sqrtBy) * cos(M_PI * (2 * i + 1) * j / (2*By));
-        }
+        for ( j = 1; j < By; j++)
+            matDct2[i][j] = (sqrt2/sqrtBy) * cos(M_PI * (2*i + 1) * j / (2*By));
     }
 
-
-    double **tdct0 = alocamd(H, W); // image transition test 1
-
+    // Image transition test 1
+    double **tdct0 = alocamd(H, W);
     for (i = 0; i < H; i++)
-        for (j = 0; j < W; j++){
-            tdct0[i][j] = 0.0;
-            tdct1[i][j] = 0.0;
-        }
+        for (j = 0; j < W; j++)
+            tdct0[i][j] = tdct1[i][j] = 0.0;
 
-    int nbx, nby;       // indice du bloc courant
-    int a, b;           // indice du pixel courant dans le bloc
+    int nbx, nby;   // Indice du bloc courant
+    int a, b;       // Indice du pixel courant dans le bloc
 
     for (nbx = 0; nbx < (H/Bx); nbx++)
         for (nby = 0; nby < (W/By); nby++)
@@ -244,55 +289,48 @@ void My_dct2dim_bloc(double **xd, double **tdct1, int H, int W, int Bx, int By, 
                     for (i = 0; i < Bx; i++)
                         tdct0[a+(nbx*Bx)][b+(nby*By)] += matDct1[a][i] * xd[i+(nbx*Bx)][b+(nby*By)];
 
-
     for (nbx = 0; nbx < (H/Bx); nbx++)
         for (nby = 0; nby < (W/By); nby++)
             for (a = 0; a < Bx; a++)
                 for (b = 0; b <By; b++)
                     for (j = 0; j < Bx; j++)
                         tdct1[a+(nbx*Bx)][b+(nby*By)] += tdct0[a+(nbx*Bx)][j+(nby*By)] * matDct2[j][b];
+
 }
 
-
-void My_dct2dim_bloc_inv(double **tdct1, double **xrecd1, int H, int W, int Bx, int By){
+void My_dct2dim_bloc_inv(double **tdct1, double **xrecd1, int H, int W, int Bx, int By) {
 
     // Valeurs utiles : on ne les calcule qu'une fois car long
-    double sqrtBx = sqrt((double)Bx);
-    double sqrtBy = sqrt((double)By);
+    double sqrtBx = sqrt((double)(Bx));
+    double sqrtBy = sqrt((double)(By));
     double sqrt2 = sqrt(2.);
 
-    // Matrices utiles
-    int i, j;
-    double **matDct1Inv = alocamd(Bx, By);      // transposée de matDct1 par bloc
+    int i, j;    // Pour la définition des matrices utiles
 
-    for ( j = 0; j < By; j++)
+    // Trasnposée de matDct1 par blocs
+    double **matDct1Inv = alocamd(Bx, By);
+    for (j = 0; j < By; j++)
         matDct1Inv[j][0] = 1 / sqrtBx;
-    for( i = 1 ; i < Bx; i++){
-        for(j = 0; j < By; j++){
-            matDct1Inv[j][i]= (sqrt2 / sqrtBx) * cos(M_PI * (2 * j + 1) * i / (2*Bx));
-        }
-    }
+    for (i = 1; i < Bx; i++)
+        for(j = 0; j < By; j++)
+            matDct1Inv[j][i] = (sqrt2/sqrtBx) * cos(M_PI * (2*j + 1) * i / (2*Bx));
 
-    double **matDct2Inv = alocamd(By, Bx);      // transposée de matDct2 par bloc
-
-    for ( i = 0; i < Bx; i++){
+    // Transposée de matDct2 par bloc
+    double **matDct2Inv = alocamd(By, Bx);
+    for (i = 0; i < Bx; i++) {
         matDct2Inv[0][i] = 1/sqrtBy;
-        for ( j = 1; j < By; j++){
-            matDct2Inv[j][i]= (sqrt2 / sqrtBy) * cos(M_PI * (2 * i + 1) * j / (2*By));
-        }
+        for (j = 1; j < By; j++)
+            matDct2Inv[j][i] = (sqrt2/sqrtBy) * cos(M_PI * (2*i + 1) * j / (2*By));
     }
 
+    // Image transitoire
+    double **xrecd0 = alocamd(H, W);
+    for (i = 0; i < H; i++)
+        for (j = 0; j < W; j++)
+            xrecd0[i][j] = xrecd1[i][j] = 0.0;
 
-    double **xrecd0 = alocamd(H, W); // image transisitoire
-
-    for(i = 0; i < H; i++)
-        for(j = 0; j < W; j++) {
-            xrecd0[i][j] = 0.0;
-            xrecd1[i][j] = 0.0;
-        }
-
-    int nbx, nby;       // indice du bloc courant
-    int a, b;           // indice du pixel courant dans le bloc
+    int nbx, nby;       // Indice du bloc courant
+    int a, b;           // Indice du pixel courant dans le bloc
 
     for (nbx = 0; nbx < (H/Bx); nbx++)
         for (nby = 0; nby < (W/By); nby++)
@@ -301,28 +339,26 @@ void My_dct2dim_bloc_inv(double **tdct1, double **xrecd1, int H, int W, int Bx, 
                     for (i = 0; i < Bx; i++)
                         xrecd0[a+(nbx*Bx)][b+(nby*By)] += matDct1Inv[a][i] * tdct1[i+(nbx*Bx)][b+(nby*By)];
 
-
     for (nbx = 0; nbx < (H/Bx); nbx++)
         for (nby = 0; nby < (W/By); nby++)
             for (a = 0; a < Bx; a++)
                 for (b = 0; b <By; b++)
                     for (j = 0; j < Bx; j++)
                         xrecd1[a+(nbx*Bx)][b+(nby*By)] += xrecd0[a+(nbx*Bx)][j+(nby*By)] * matDct2Inv[j][b];
+
 }
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////        /////////////////////////////////////////////////////////////
+////////////////////////////////////////////////     MAIN     //////////////////////////////////////////////////////////
+///////////////////////////////////////////////////        /////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+int main (int argc, char *argv[]) {
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////        /////////////////////////////////////////////////////////////
-////////////////////////////////////////     MAIN     //////////////////////////////////////////////////////////
-///////////////////////////////////////////        /////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-int main (int argc, char *argv[]){
     char nom[200], nom_out[200], nom_err[300];
     char nom_out_My[200], nom_err_My[300];
-    int W, H; // les dimensions de l'image: H = Height, W = Width
+    int W, H;   // Dimensions de l'image: H = Height, W = Width
     int i, j;
 
     if (argc != 3) {
@@ -343,16 +379,16 @@ int main (int argc, char *argv[]){
     fprintf(stderr, "Width = %d  Height = %d\n", W, H);
 
     unsigned char **x = alocamuc(H, W);
-    unsigned char **xrec = alocamuc(H, W);      // image finale en utilisant la dct donnée
-    unsigned char **xrec1 = alocamuc(H, W);     // Mon image finale
+    unsigned char **xrec = alocamuc(H, W);  // Image finale en utilisant la dct donnée
+    unsigned char **xrec1 = alocamuc(H, W); // Notre image finale
 
-    double **xrecd = alocamd(H, W);     // image reconstruite
-    double **xrecd1 = alocamd(H, W);    // Mon image reconstruite
+    double **xrecd = alocamd(H, W);     // Image reconstruite
+    double **xrecd1 = alocamd(H, W);    // Notre image reconstruite
 
     lecture_pgm(nom, x);    // x contient l'image initiale
 
-    int **err = alocami(H, W);      // allocation de la matrice des erreurs de prediction
-    int **err1 = alocami(H, W);     // allocation de notre matrice des erreurs de prediction
+    int **err = alocami(H, W);      // Allocation de la matrice des erreurs de prediction
+    int **err1 = alocami(H, W);     // Allocation de notre matrice des erreurs de prediction
 
     for (i = 0; i < H; i++)
         for (j = 0; j < W; j++)
@@ -420,7 +456,6 @@ int main (int argc, char *argv[]){
     dalocd(tdct1, H);
 
 */
-
     ////////////////////////////////////////    FIN CODE DCT     /////////////////////////////////////////////////////
 
 
@@ -496,11 +531,9 @@ int main (int argc, char *argv[]){
 
     ////////////////////////////////////////     FIN DCT PAR BLOCS    /////////////////////////////////////////////////
 
-
     dalocuc(x, H);
     dalocuc(xrec, H);
     daloci(err, H);
-
-    return 1;
+    return EXIT_FAILURE;
 }
 
